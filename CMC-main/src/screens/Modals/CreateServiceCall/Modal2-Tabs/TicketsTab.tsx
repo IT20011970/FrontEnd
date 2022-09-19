7d26c7b95e16c1c2c97d73dc65f1b43b1becd2ef
unknown <gayanpoornima@gmail.com>
2022-09-19 14:53:02 +0530

    new report

--- CMC-main/src/screens/Modals/CreateServiceCall/Modal2-Tabs/TicketsTab.tsx
+++ CMC-main/src/screens/Modals/CreateServiceCall/Modal2-Tabs/TicketsTab.tsx
@@ -67,14 +67,14 @@ const StyledTableRow = styled(TableRow)(({ theme }) => ({
 }));
 
 const createData = (
-  date: Date,
-  time: string,
-  engineer: string,
-  priority: string,
-  plannedStart: Date,
-  recurrence: string,
-  content: string,
-  more: string
+    date: Date,
+    time: string,
+    engineer: string,
+    priority: string,
+    plannedStart: Date,
+    recurrence: string,
+    content: string,
+    more: string
 ) => {
   return {
     date,
@@ -91,16 +91,16 @@ const createData = (
 const rows: any = [];
 for (var i = 0; i < 5; i++) {
   rows.push(
-    createData(
-      new Date(),
-      "10:23:14 AM",
-      "Engineer E",
-      "Priority P",
-      new Date(),
-      "Recurrence R",
-      "Content C",
-      "More M"
-    )
+      createData(
+          new Date(),
+          "10:23:14 AM",
+          "Engineer E",
+          "Priority P",
+          new Date(),
+          "Recurrence R",
+          "Content C",
+          "More M"
+      )
   );
 }
 
@@ -113,17 +113,17 @@ const TicketsTab = (props: any) => {
   const [rowsPerPage, setRowsPerPage] = React.useState(10);
 
   const emptyRows =
-    page > 0 ? Math.max(0, (1 + page) * rowsPerPage - rows.length) : 0;
+      page > 0 ? Math.max(0, (1 + page) * rowsPerPage - rows.length) : 0;
 
   const handleChangePage = (
-    event: React.MouseEvent<HTMLButtonElement> | null,
-    newPage: number
+      event: React.MouseEvent<HTMLButtonElement> | null,
+      newPage: number
   ) => {
     setPage(newPage);
   };
 
   const handleChangeRowsPerPage = (
-    event: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
+      event: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
   ) => {
     setRowsPerPage(parseInt(event.target.value, 10));
     setPage(0);
@@ -133,170 +133,170 @@ const TicketsTab = (props: any) => {
     setTicketList([
       ...ticketList,
       createData(
-        new Date(),
-        "10:23:14 AM",
-        "Engineer E",
-        "Priority P",
-        new Date(),
-        "Recurrence R",
-        "Content C",
-        "More M"
+          new Date(),
+          "10:23:14 AM",
+          "Engineer E",
+          "Priority P",
+          new Date(),
+          "Recurrence R",
+          "Content C",
+          "More M"
       ),
     ]);
   };
 
   return (
-    <Box sx={{ flexGrow: 1 }}>
-      <TableContainer component={Paper} sx={{ boxShadow: "none" }}>
-        <Table sx={{ width: "100%" }} aria-label="custom pagination table">
-          <TableHead>
-            <TableRow>
-              <StyledTableCell
-                sx={{
-                  borderLeft: "none",
-                }}
-              >
-                Date
-              </StyledTableCell>
-              <StyledTableCell
-                sx={{
-                  borderLeft: "1px solid rgba(0, 65, 102, 0.2);",
-                }}
-              >
-                Time
-              </StyledTableCell>
-              <StyledTableCell
-                sx={{
-                  borderLeft: "1px solid rgba(0, 65, 102, 0.2);",
-                }}
-              >
-                Engineer
-              </StyledTableCell>
-              <StyledTableCell
-                sx={{
-                  borderLeft: "1px solid rgba(0, 65, 102, 0.2);",
-                }}
-              >
-                Priority
-              </StyledTableCell>
-              <StyledTableCell
-                sx={{
-                  borderLeft: "1px solid rgba(0, 65, 102, 0.2);",
-                }}
-              >
-                Planned Start
-              </StyledTableCell>
-              <StyledTableCell
-                sx={{
-                  borderLeft: "1px solid rgba(0, 65, 102, 0.2);",
-                }}
-              >
-                Recurrence
-              </StyledTableCell>
-              <StyledTableCell
-                sx={{
-                  borderLeft: "1px solid rgba(0, 65, 102, 0.2);",
-                }}
-              >
-                Content
-              </StyledTableCell>
-              <StyledTableCell
-                sx={{
-                  borderLeft: "1px solid rgba(0, 65, 102, 0.2);",
-                }}
-              >
-                More
-              </StyledTableCell>
-            </TableRow>
-          </TableHead>
-          <TableBody>
-            {(rowsPerPage > 0
-              ? ticketList.slice(
-                  page * rowsPerPage,
-                  page * rowsPerPage + rowsPerPage
-                )
-              : rows
-            ).map((row: CreateServiceCallTicketData, i: number) => (
-              <StyledTableRow key={Math.random()}>
+      <Box sx={{ flexGrow: 1 }}>
+        <TableContainer component={Paper} sx={{ boxShadow: "none" }}>
+          <Table sx={{ width: "100%" }} aria-label="custom pagination table">
+            <TableHead>
+              <TableRow>
                 <StyledTableCell
-                  sx={{
-                    borderLeft: "none",
-                  }}
+                    sx={{
+                      borderLeft: "none",
+                    }}
                 >
-                  {row.date.toString().substring(0, 24)}
+                  Date
                 </StyledTableCell>
                 <StyledTableCell
-                  sx={{
-                    borderLeft: "1px solid rgba(0, 65, 102, 0.2);",
-                  }}
+                    sx={{
+                      borderLeft: "1px solid rgba(0, 65, 102, 0.2);",
+                    }}
                 >
-                  {row.time}
+                  Time
                 </StyledTableCell>
                 <StyledTableCell
-                  sx={{
-                    borderLeft: "1px solid rgba(0, 65, 102, 0.2);",
-                  }}
+                    sx={{
+                      borderLeft: "1px solid rgba(0, 65, 102, 0.2);",
+                    }}
                 >
-                  {row.engineer + Math.round(i * Math.random() * 100)}
+                  Engineer
                 </StyledTableCell>
                 <StyledTableCell
-                  sx={{
-                    borderLeft: "1px solid rgba(0, 65, 102, 0.2);",
-                  }}
+                    sx={{
+                      borderLeft: "1px solid rgba(0, 65, 102, 0.2);",
+                    }}
                 >
-                  {row.priority + Math.round(i * Math.random() * 100)}
+                  Priority
                 </StyledTableCell>
                 <StyledTableCell
-                  sx={{
-                    borderLeft: "1px solid rgba(0, 65, 102, 0.2);",
-                  }}
+                    sx={{
+                      borderLeft: "1px solid rgba(0, 65, 102, 0.2);",
+                    }}
                 >
-                  {row.plannedStart.toString().substring(0, 24)}
+                  Planned Start
                 </StyledTableCell>
                 <StyledTableCell
-                  sx={{
-                    borderLeft: "1px solid rgba(0, 65, 102, 0.2);",
-                  }}
+                    sx={{
+                      borderLeft: "1px solid rgba(0, 65, 102, 0.2);",
+                    }}
                 >
-                  {row.recurrence + Math.round(i * Math.random() * 100)}
+                  Recurrence
                 </StyledTableCell>
                 <StyledTableCell
-                  sx={{
-                    borderLeft: "1px solid rgba(0, 65, 102, 0.2);",
-                  }}
+                    sx={{
+                      borderLeft: "1px solid rgba(0, 65, 102, 0.2);",
+                    }}
                 >
-                  {row.content + Math.round(i * Math.random() * 100)}
+                  Content
                 </StyledTableCell>
                 <StyledTableCell
-                  sx={{
-                    borderLeft: "1px solid rgba(0, 65, 102, 0.2);",
-                  }}
+                    sx={{
+                      borderLeft: "1px solid rgba(0, 65, 102, 0.2);",
+                    }}
                 >
-                  {row.more + Math.round(i * Math.random() * 100)}
+                  More
                 </StyledTableCell>
-              </StyledTableRow>
-            ))}
-          </TableBody>
-        </Table>
-      </TableContainer>
-      <Box sx={{ flexGrow: 1, py: 1 }}>
-        <Grid container spacing={10}>
-          <Grid item xs={8} md={10}></Grid>
-          <Grid item xs={4} md={2}>
-            <ModalButton
-              variant="contained"
-              className="ModalCommonButton"
-              onClick={() => setOpenModal(true)}
-              sx={{ width: "250px", mt: 2 }}
-            >
-              Create New Ticket
-            </ModalButton>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {(rowsPerPage > 0
+                      ? ticketList.slice(
+                          page * rowsPerPage,
+                          page * rowsPerPage + rowsPerPage
+                      )
+                      : rows
+              ).map((row: CreateServiceCallTicketData, i: number) => (
+                  <StyledTableRow key={Math.random()}>
+                    <StyledTableCell
+                        sx={{
+                          borderLeft: "none",
+                        }}
+                    >
+                      {row.date.toString().substring(0, 24)}
+                    </StyledTableCell>
+                    <StyledTableCell
+                        sx={{
+                          borderLeft: "1px solid rgba(0, 65, 102, 0.2);",
+                        }}
+                    >
+                      {row.time}
+                    </StyledTableCell>
+                    <StyledTableCell
+                        sx={{
+                          borderLeft: "1px solid rgba(0, 65, 102, 0.2);",
+                        }}
+                    >
+                      {row.engineer + Math.round(i * Math.random() * 100)}
+                    </StyledTableCell>
+                    <StyledTableCell
+                        sx={{
+                          borderLeft: "1px solid rgba(0, 65, 102, 0.2);",
+                        }}
+                    >
+                      {row.priority + Math.round(i * Math.random() * 100)}
+                    </StyledTableCell>
+                    <StyledTableCell
+                        sx={{
+                          borderLeft: "1px solid rgba(0, 65, 102, 0.2);",
+                        }}
+                    >
+                      {row.plannedStart.toString().substring(0, 24)}
+                    </StyledTableCell>
+                    <StyledTableCell
+                        sx={{
+                          borderLeft: "1px solid rgba(0, 65, 102, 0.2);",
+                        }}
+                    >
+                      {row.recurrence + Math.round(i * Math.random() * 100)}
+                    </StyledTableCell>
+                    <StyledTableCell
+                        sx={{
+                          borderLeft: "1px solid rgba(0, 65, 102, 0.2);",
+                        }}
+                    >
+                      {row.content + Math.round(i * Math.random() * 100)}
+                    </StyledTableCell>
+                    <StyledTableCell
+                        sx={{
+                          borderLeft: "1px solid rgba(0, 65, 102, 0.2);",
+                        }}
+                    >
+                      {row.more + Math.round(i * Math.random() * 100)}
+                    </StyledTableCell>
+                  </StyledTableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </TableContainer>
+        <Box sx={{ flexGrow: 1, py: 1 }}>
+          <Grid container spacing={10}>
+            <Grid item xs={8} md={10}></Grid>
+            <Grid item xs={4} md={2}>
+              <ModalButton
+                  variant="contained"
+                  className="ModalCommonButton"
+                  onClick={() => setOpenModal(true)}
+                  sx={{ width: "250px", mt: 2 }}
+              >
+                Create New Ticket
+              </ModalButton>
+            </Grid>
           </Grid>
-        </Grid>
-      </Box>
+        </Box>
 
-      <CreateNewTicketModal open={openModal} setOpen={setOpenModal} />
-    </Box>
+        <CreateNewTicketModal open={openModal} setOpen={setOpenModal} />
+      </Box>
   );
 };
 
